e3ebc3db4c08dcc94aa5ff5cfff7531b7731f1d5
luckylavesh <lsoni3446@gmail.com>
2022-12-10 23:39:21 +0530

    UPDATED

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+// These credentials were created only for development purposes
+module.exports = {
+    oldMLABMongoURI: '',
+    mongoURI: 'mongodb+srv://laveshsoni:7470350883@lavesh.yr4g8ax.mongodb.net/?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
+};
